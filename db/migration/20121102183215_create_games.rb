7aad7c705c36b1c8a8d3d7e0b5ea75d3f6f86ced
Jack Nolan <jack@jacknolan.com>
2012-11-02 14:50:38 -0400

    orginizational changes

---
+++ db/migration/20121102183215_create_games.rb
@@ -0,0 +1,14 @@
+require_relative "../../config/databaseinfo"
+class CreateGames < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+      t.decimal :price
+      t.integer :metascore
+      t.string :name
+      t.date :released
+
+      t.timestamps
+    end
+  end
+end
+CreateGames.new.change
